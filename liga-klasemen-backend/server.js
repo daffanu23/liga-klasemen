72f981af8c7ba378c308c5d5b5a1481985e66c65
daffanu23 <daffanugraha058@gmail.com>
2025-06-26 12:02:35 +0700

    feat: Testing

--- liga-klasemen-backend/server.js
+++ liga-klasemen-backend/server.js
@@ -1,3 +1,10 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+const supabase = createClient(
+    "https://kwjjrtfukjybwqwdequl.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3ampydGZ1a2p5Yndxd2RlcXVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4MzAyMjQsImV4cCI6MjA2NjQwNjIyNH0.iAYjFovdfeG7P6lwHwNOlO3Apww1uoQ8jxzu3mnT8d8"
+  );
+
 const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
